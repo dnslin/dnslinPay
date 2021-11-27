772cf04678f26957637dc181a387c3373c927277
dnslin <1636988859@qq.com>
2021-11-27 23:20:05 +0800

    增加回调验签

--- src/main/java/com/dnslin/pay/controller/PayController.java
+++ src/main/java/com/dnslin/pay/controller/PayController.java
@@ -1,6 +1,9 @@
 package com.dnslin.pay.controller;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.fastjson.JSON;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.dnslin.pay.config.AlipayConfig;
 import com.dnslin.pay.model.GoodsDto;
 import com.dnslin.pay.result.R;
 import com.dnslin.pay.result.ResponseEnum;
@@ -10,6 +13,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 public class PayController {
   @Autowired private PayService service;
 
+  @Autowired private AlipayConfig config;
 
   @PostMapping("/pay")
   /**
@@ -34,29 +42,52 @@ public class PayController {
     log.info("订单实体类-->{}", goodsDto);
     if (BeanUtil.isNotEmpty(goodsDto)) {
       try {
-        if (StringUtils.isNotBlank(goodsDto.getEmail())){
+        if (StringUtils.isNotBlank(goodsDto.getEmail())) {
           String regex = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
           boolean matches = goodsDto.getEmail().matches(regex);
-          if(matches){
+          if (matches) {
             url = service.createOrder(goodsDto);
-          }else {
+          } else {
             log.error("邮箱格式错误");
             msg = "邮箱格式错误";
             throw new IllegalStateException();
           }
-        }else{
+        } else {
           log.error("邮箱为空");
           msg = "邮箱信息为空不发送通知邮件";
           throw new IllegalStateException();
         }
       } catch (Exception e) {
         url = service.createOrder(goodsDto);
-        return new R("201",msg, url);
+        return new R("201", msg, url);
       }
     } else {
       log.error("订单实体类-->{}", goodsDto);
-      return new R("400", "订单信息为空",null);
+      return new R("400", "订单信息为空", null);
     }
     return new R(ResponseEnum.SUCCESS, url);
+  }
+
+  @PostMapping("/notify")
+  public String tradeNotify(HttpServletRequest request) {
+    Map<String, String[]> parameterMap = request.getParameterMap();
+    Map<String, String> params = new HashMap<>(32);
+    parameterMap.forEach((key, val) -> params.put(key, String.join(",", val)));
+    log.info("============>>> trade-notifyparams:{}", JSON.toJSONString(params));
+    boolean check;
+    try {
+      // 必须要验签
+      check =
+          AlipaySignature.rsaCheckV1(
+              params, config.getAlipayPublicKey(), AlipayConfig.CHARSET, AlipayConfig.SIGN_TYPE);
+      log.info("============>>> check: [{}]", check);
+    } catch (Exception e) {
+      log.warn("tradeNotify exception", e);
+      return "failure";
+    }
+    if (!check) {
+      return "rsaCheckV1 = false";
+    }
+    return "success";
   }
 }
---
+++ src/main/java/com/dnslin/pay/service/MallService.java
@@ -0,0 +1,17 @@
+package com.dnslin.pay.service;/**
+ * @author: DnsLin
+ * @Title: MallService
+ * @ProjectName: dnslinPay
+ * @Description:
+ * @date: 2021/11/27 16:53
+ */
+
+/**
+ * @author: DnsLin
+ * @Title: MallService
+ * @ProjectName: dnslinPay
+ * @Description:
+ * @date: 2021/11/27 16:53
+ */
+public class MallService {
+}
