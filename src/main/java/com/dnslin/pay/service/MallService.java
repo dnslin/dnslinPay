118a48b1c7927fdb83759a67a77de7a95d99fc5a
dnslin <1636988859@qq.com>
2021-11-28 14:11:36 +0800

    增加邮件模版

--- src/main/java/com/dnslin/pay/service/MallService.java
+++ src/main/java/com/dnslin/pay/service/MallService.java
@@ -1,10 +1,12 @@
-package com.dnslin.pay.service;/**
- * @author: DnsLin
- * @Title: MallService
- * @ProjectName: dnslinPay
- * @Description:
- * @date: 2021/11/27 16:53
- */
+package com.dnslin.pay.service;
+
+import com.dnslin.pay.utils.MailUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+
+import org.springframework.stereotype.Service;
 
 /**
  * @author: DnsLin
@@ -13,5 +15,12 @@ package com.dnslin.pay.service;/**
  * @Description:
  * @date: 2021/11/27 16:53
  */
+@Service
 public class MallService {
+    @Autowired
+    private MailUtil mailUtil;
+
+    private final String receiver = "wgt161110527@163.com";
+
+
 }
--- src/test/java/com/dnslin/pay/test.java
+++ src/test/java/com/dnslin/pay/test.java
@@ -4,10 +4,13 @@ package com.dnslin.pay;
  * @date: 2021/11/24 21:56
  */
 
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.NumberUtil;
 import com.alipay.api.AlipayApiException;
 import com.dnslin.pay.model.GoodsDto;
 import com.dnslin.pay.service.PayService;
+import com.dnslin.pay.utils.MailUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: DnsLin @Title: test @ProjectName: dnslinPay @Description:
@@ -22,9 +29,15 @@ import java.math.BigDecimal;
  */
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@Slf4j
 public class test {
   @Autowired
   private PayService pay;
+
+  @Autowired
+  private MailUtil mailUtil;
+
+  private final String receiver = "3164419779@qq.com";
 
   @Test
   public void test() throws AlipayApiException {
@@ -35,5 +48,29 @@ public class test {
   @Test
   public void test02(){
     System.out.println(NumberUtil.generateRandomNumber(10, 100000, 3).toString());
+  }
+
+  @Test
+  public void test03(){
+    //发送邮件
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+    String subject = "SecondGoodTrade用户注册";
+    String emailTemplate = "registerTemplate";
+    String code = IdUtil.simpleUUID().toString();
+    Map<String, Object> dataMap = new HashMap<>();
+    dataMap.put("OrderContents", receiver);
+    dataMap.put("OrderId", code);
+    dataMap.put("OrderPrice", sdf.format(new Date()));
+    dataMap.put("creadTime", sdf.format(new Date()));
+    dataMap.put("payTime", sdf.format(new Date()));
+    dataMap.put("remask", sdf.format(new Date()));
+    try {
+      log.info("进入方法");
+      mailUtil.sendTemplateMail(receiver, subject, emailTemplate, dataMap);
+    } catch (Exception e) {
+      log.error("退出方法");
+      e.printStackTrace();
+      return;
+    }
   }
 }
